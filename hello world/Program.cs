7914f8352320d6a29129f2f46b478a689c61e77b
Lluis planas <gam_lluis@hotmail.com>
2020-12-22 17:19:49 +0100

    Agregar archivos de proyecto.

---
+++ hello world/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey() ; //Para la ejecucion hasta presionar tecla
+        }
+    }
+}
